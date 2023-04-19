a9308d026d0da48461f41479c7aa60ee5effa765
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-19 09:46:39 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -1,6 +1,35 @@
 package q1
 
-func CalculateDiscount(currentPurchase float64, purchaseHistory []float64) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+import (
+	"fmt"
+)
+
+func verificarElegebilidadeDesconto(valordaCompraAtual float64, historicodeCompras []float64) (float64, error) {
+	if valordaCompraAtual <= 0 {
+		return 0, fmt.Errorf("Valor da compra é invalido")
+	}
+	valorTotalHistorico := 0.0
+	for _, valorDeCadaCompra := range historicodeCompras {
+		valorTotalHistorico += valorDeCadaCompra
+	}
+	desconto := 0.0
+	media := 0.0
+	if valorTotalHistorico > 0 {
+		media = valorTotalHistorico / float64(len(historicodeCompras))
+	}
+
+	if len(historicodeCompras) == 0 {
+		desconto = valordaCompraAtual * 0.1
+	} else if media > 1000 {
+		desconto = valordaCompraAtual * 0.2
+	} else if valorTotalHistorico > 1000 {
+		desconto = valordaCompraAtual * 0.10
+	} else if valorTotalHistorico <= 1000 && valorTotalHistorico > 500 {
+		desconto = valordaCompraAtual * 0.05
+	} else if valorTotalHistorico <= 500 {
+		desconto = valordaCompraAtual * 0.02
+	}
+
+	return desconto, nil
+
 }
