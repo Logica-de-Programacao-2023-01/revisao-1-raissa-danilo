fd64ca7a9fca3135b0ed9130d80c0359c06c4694
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-21 13:23:32 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -12,24 +12,27 @@ func verificarElegebilidadeDesconto(valordaCompraAtual float64, historicodeCompr
 	for _, valorDeCadaCompra := range historicodeCompras {
 		valorTotalHistorico += valorDeCadaCompra
 	}
-	desconto := 0.0
 	media := 0.0
 	if valorTotalHistorico > 0 {
 		media = valorTotalHistorico / float64(len(historicodeCompras))
 	}
 
-	 if media > 1000 {
-		desconto = valordaCompraAtual * 0.2
-	} else if valorTotalHistorico > 1000 {
-		desconto = valordaCompraAtual * 0.10
-	} else if valorTotalHistorico <= 1000 && valorTotalHistorico > 500 {
-		desconto = valordaCompraAtual * 0.05
-	} else if valorTotalHistorico <= 500 {
-		desconto = valordaCompraAtual * 0.02
-	}
-	if len(historicodeCompras) == 0 {
-		desconto = valordaCompraAtual * 0.1
+	var discount float64
+	if valorTotalHistorico > 1000 {
+		discount = 0.1
+	} else if valorTotalHistorico > 500 {
+		discount = 0.05
+	} else {
+		discount = 0.02
 	}
 
-	return desconto, nil
+	if len(historicodeCompras) == 0 {
+		discount = 0.1
+	}
+	
+	if media > 1000 {
+		discount = 0.2
+	}
+	
+	return discount * valordaCompraAtual, nil
 }
