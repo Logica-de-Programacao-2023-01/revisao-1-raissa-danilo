19f2f44af5dd7eacc8d4eb0c8a52f8a83c5a85c5
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-21 21:12:05 -0300

    Update q1.go

--- q1/q1.go
+++ q1/q1.go
@@ -6,33 +6,32 @@ import (
 
 func CalculateDiscount(valorCompraAtual float64, historicoCompras []float64) (float64, error) {
 	if valorCompraAtual <= 0 {
-		return 0, fmt.Errorf("Valor da compra invalido")
+		return 0, fmt.Errorf("Valor da compra invalido.")
 	}
 
-	desconto := 0.0
 	media := 0.0
+	desconto := 0.0
 
-	valorTotalHistorico := 0.0
-	for _, valorCompras := range historicoCompras {
-		valorTotalHistorico += valorCompras
+	valorHistoricoTotal := 0.0
+	for _, valorCadaCompra := range historicoCompras {
+		valorHistoricoTotal += valorCadaCompra
 	}
 
-	if valorTotalHistorico > 0 {
-		media = valorTotalHistorico / float64(len(historicoCompras))
+	if valorHistoricoTotal > 0 {
+		media = valorHistoricoTotal / float64(len(historicoCompras))
 	}
 
 	if len(historicoCompras) == 0 {
 		desconto = valorCompraAtual * 0.1
 	} else if media > 1000 {
 		desconto = valorCompraAtual * 0.2
-	} else if valorTotalHistorico > 1000 {
+	} else if valorHistoricoTotal > 1000 {
 		desconto = valorCompraAtual * 0.1
-	} else if valorTotalHistorico <= 1000 &&, valorTotalHistorico > 500 {
+	} else if valorHistoricoTotal <= 1000 && valorHistoricoTotal > 500 {
 		desconto = valorCompraAtual * 0.05
-	} else if valorTotalHistorico <= 500 {
-		desconto = valorTotalHistorico * 0.02
+	} else if valorHistoricoTotal <= 500 {
+		desconto = valorCompraAtual * 0.02
 	}
-	
-	
-	return desconto, nil 
+
+	return desconto, nil
 }
