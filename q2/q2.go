a9cd7958b137fe1e6364ce641a6436bac9a733f1
daanilow <126572058+daanilow@users.noreply.github.com>
2023-04-23 13:37:11 -0300

    Update q2.go

--- q2/q2.go
+++ q2/q2.go
@@ -1,6 +1,27 @@
 package q2
 
+import (
+	"errors"
+	"strings"
+)
+
 func AverageLettersPerWord(text string) (float64, error) {
-	// Implemente sua solução aqui
-	return 0, nil
+	caracteresEspeciais := []string{".", ",", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+
+	for _, caracteres := range caracteresEspeciais {
+		text = strings.ReplaceAll(text, caracteres, "")
+	}
+
+	palavras := strings.Fields(text)
+	if len(palavras) == 0 {
+		return 0, errors.New("texto está vazio")
+	}
+
+	var numeroTotalDeLetras = 0
+
+	for _, palavra := range palavras {
+		numeroTotalDeLetras += len(palavra)
+	}
+
+	return float64(numeroTotalDeLetras) / float64(len(palavras)), nil
 }
